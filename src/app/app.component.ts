28107729cffad211cf11b8a34493aae68e225eea
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-05-25 00:52:12 -0600

    portafolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './sections/about/about.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
+import { SkillsComponent } from './sections/skills/skills.component';
+import { ContactComponent } from './sections/contact/contact.component';
+import { PortadaComponent } from './sections/portada/portada.component';
+import { ExperienciaComponent } from './sections/experiencia/experiencia.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+    PortadaComponent,
+    ExperienciaComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'mi-portafolio';
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit(): void {
+    const background = this.el.nativeElement.querySelector('.star-background');
+
+    // 🌟 Crear estrellas fijas
+    for (let i = 0; i < 200; i++) {
+      const star = this.renderer.createElement('div');
+      this.renderer.addClass(star, 'star');
+      this.renderer.setStyle(star, 'top', `${Math.random() * 100}%`);
+      this.renderer.setStyle(star, 'left', `${Math.random() * 100}%`);
+      this.renderer.setStyle(
+        star,
+        'animationDuration',
+        `${1 + Math.random() * 2}s`
+      );
+      this.renderer.appendChild(background, star);
+    }
+
+    // 💫 Estrellas fugaces
+    setInterval(() => {
+      const shootingStar = this.renderer.createElement('div');
+      this.renderer.addClass(shootingStar, 'shooting-star');
+      this.renderer.setStyle(
+        shootingStar,
+        'left',
+        `${Math.random() * window.innerWidth}px`
+      );
+      this.renderer.setStyle(shootingStar, 'top', `${Math.random() * 100}px`);
+      this.renderer.appendChild(background, shootingStar);
+
+      setTimeout(() => {
+        this.renderer.removeChild(background, shootingStar);
+      }, 3000);
+    }, 5000);
+  }
 }
---
+++ src/app/sections/projects/projects.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  imports: [CommonModule],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.css',
+})
+export class ProjectsComponent {
+  proyectos = [
+    {
+      titulo: 'dvinternet',
+      descripcion:
+        'Sitio web informativo para una empresa de internet, desarrollado con HTML, CSS y PHP, con diseño responsivo y formulario de contacto funcional.',
+      imagen: '/assets/img/dvinternet.png',
+      tecnologias: ['php', 'css', 'html', 'Mysql', 'Bootstrap'],
+      fecha: '20 mayo 2022',
+      demo: '#',
+      github: '#',
+    },
+    {
+      titulo: 'dvinternet',
+      descripcion:
+        'Aplicación web desarrollada con php y mysql para gestionar clientes, servicios, pagos y reportes administrativos de la empresa.',
+      imagen: '/assets/img/dv-system.png',
+      tecnologias: ['php', 'css', 'html', 'mysql', 'Bootstrap'],
+      fecha: '22 abril 2022',
+      demo: '#',
+      github: '#',
+    },
+    {
+      titulo: 'Roamsuite.com',
+      descripcion:
+        'Plataforma web creada con Angular y Laravel para gestionar reservas, disponibilidad de habitaciones y administración de hoteles en línea.',
+      imagen: 'assets/img/roamsuite.png',
+      tecnologias: ['Angular', 'Mysql', 'Laravel', 'Bootstrap'],
+      fecha: '15 jul 2023',
+      demo: '#',
+      github: '#',
+    },
+    {
+      titulo: 'Promptopia',
+      descripcion: 'Plataforma para compartir y descubrir prompts.',
+      imagen: 'assets/img/promptopia.jpg',
+      tecnologias: ['Next.js', 'MongoDB', 'Vercel'],
+      fecha: '28 jun 2023',
+      demo: '#',
+      github: '#',
+    },
+  ];
+}
